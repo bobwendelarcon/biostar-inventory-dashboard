3711b033a25e6daad468ea3f56ce5d6470a9e746
bob arcon <bobwendelarcon@gmail.com>
2026-06-16 12:47:43 +0800

    adjusted css

--- biostar_inventory_dashboard/obj/Debug/net8.0/biostar_inventory_dashboard.AssemblyInfo.cs
+++ biostar_inventory_dashboard/obj/Debug/net8.0/biostar_inventory_dashboard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("biostar_inventory_dashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ace6d84c0b184f69957bfcf0909dc5d87f7e54a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb287ec1bd49f60cbc59055684e7738180ac0bb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("biostar_inventory_dashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("biostar_inventory_dashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
