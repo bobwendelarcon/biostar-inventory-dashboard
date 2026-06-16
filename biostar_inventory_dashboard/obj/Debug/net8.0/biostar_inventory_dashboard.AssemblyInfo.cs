fb287ec1bd49f60cbc59055684e7738180ac0bb4
bob arcon <bobwendelarcon@gmail.com>
2026-06-16 12:36:36 +0800

    extend dailyorder list

--- biostar_inventory_dashboard/obj/Debug/net8.0/biostar_inventory_dashboard.AssemblyInfo.cs
+++ biostar_inventory_dashboard/obj/Debug/net8.0/biostar_inventory_dashboard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("biostar_inventory_dashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36caf85f82e916d4eb500d46785fac00ec5b7d36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ace6d84c0b184f69957bfcf0909dc5d87f7e54a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("biostar_inventory_dashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("biostar_inventory_dashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
