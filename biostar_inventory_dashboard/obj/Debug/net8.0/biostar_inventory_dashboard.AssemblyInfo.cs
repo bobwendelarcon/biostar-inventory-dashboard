d0c77dbe7ede99a010a88a611d7de02adbc139bc
bob arcon <bobwendelarcon@gmail.com>
2026-09-01 16:03:08 +0800

    added dashboard in usermanagement

--- biostar_inventory_dashboard/obj/Debug/net8.0/biostar_inventory_dashboard.AssemblyInfo.cs
+++ biostar_inventory_dashboard/obj/Debug/net8.0/biostar_inventory_dashboard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("biostar_inventory_dashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e73bed8b15e776da65a7a9d69a2778eb44b6ae5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29e3510f99d8c16295eefaa5135ec5570ae6d7e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("biostar_inventory_dashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("biostar_inventory_dashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
